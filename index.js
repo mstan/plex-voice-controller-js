fb1cbe9faaf0e6e1006a8134fc0d1634dd6e1c76
mstan <1379tech@gamil.com>
2020-03-23 21:38:45 -0700

    Clean up code commenting, spacing, files

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-require('dotenv').config();
-let debug = require('debug')('plex-controller:main');
-let PlexAPI = require('plex-api');
-let { actionManager, serverManager } = require('./lib');
+require('dotenv').config(); // environment variable loading from file
+let debug = require('debug')('plex-controller:main'); // debug logger
+let PlexAPI = require('plex-api'); // plex API library
+
+// library files
+let { actionManager, serverManager } = require('./lib'); 
 let { performAction } = actionManager;
 let { getServers } = serverManager;
 
+// Plex parameters for PlexAPI object
 const PLEX_PARAMETERS = {
 	hostname: process.env.PLEX_HOSTNAME,
 	username: process.env.PLEX_USERNAME,
@@ -17,66 +20,68 @@ const PLEX_PARAMETERS = {
 }
 
 
+// Function to prepare Plex server
 async function setup() {
 	return new Promise( async (resolve,reject) => {
-		let plexClient = await new PlexAPI(PLEX_PARAMETERS);
+		let plexClient = await new PlexAPI(PLEX_PARAMETERS); // create plex server
+
+		/*
+		  By default, we expect a server machine ID for building queries with.
+		  It's possible the user may not have set one yet, 
+		*/ 
 		if(!process.env.PLEX_DEFAULT_SERVER_MACHINE_ID) {
 			debug('Warning: No Default Server was set. Consider setting a server below from the following Server IDs in your .env file')
 			let servers = await getServers(plexClient);
 
+			// invalid response or no servers found
 			if(!servers || servers.length == 0) {
 				throw new Error("No associated servers were found");
 				process.exit(1);
 			}
 
+			/*
+				List available servers so a default one can be set
+			*/
 			debug('Available servers');
 			for(server in servers) {
 				debug(`[${ servers[server].name }] ${ servers[server].machineIdentifier  }`)
 			}
+
+			/*
+				Error out if more than one server is found and no default one is set.
+				We intentionally error AFTER listing servers so a user can check logs and pull a server ID out to set a default one
+			*/
+			if(servers.length > 1) { 
+				throw new Error("Multiple servers were found, but no default server is set.");
+				process.exit(1);
+			}
+
+
+			// If we got here, we only found one server. Even if no default exists, let's set it as the active server for this instance.
 			debug(`Defaulting to ${servers[0].name} [${servers[0].machineIdentifier}]`)
 			process.env.PLEX_DEFAULT_SERVER_MACHINE_ID = servers[0].machineIdentifier;
-
 			process.env.PLEX_API_CLIENT = plexClient;
 		}
+
 		resolve(plexClient);
 	})
 }
 
-async function voiceCommand(client,options) {
-	return await performAction(client, options);
-}
-
-
 // Example of an options payload to be passed
-
 /*
 let options = {
-	//targetClientName: "LG 75SK8070AUB", // OPTIONAL: DEBUG FOR NOW, to be abstracted later to various services
+	//targetClient: "LG 75SK8070AUB", // OPTIONAL
 	targetMediaName: "tom and jerry",
-	targetMediaTypes: ["show",],
+	targetMediaTypes: ["show"],
 	shuffle: 0, // 
 	action: "shuffle"
 }
-
-
-performAction(plexClient, options).then((response) => {
-	if(response && response.error) {
-		debug('something went wrong', response.error);
-	} else {
-		debug('success!', response);
-	}
-})
-.catch((error) => {
-	debug(error);
-	return { error };	
-})
 */
 
-/*
-setup().then((result) => {
-	return result
-})
-*/
+// Wrapper function of perform action. Consider adding more unique voiceCommand functionality here.
+async function voiceCommand(client,options) {
+	return await performAction(client, options);
+}
 
 module.exports = {
 	setup,
